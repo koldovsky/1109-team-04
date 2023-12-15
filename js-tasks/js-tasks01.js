ac382d9b133c8218788a90dec364ee40a8397c0b
Sviatoslav Nadorozhnyi <104847887+SvNadorozhnyi@users.noreply.github.com>
2023-12-15 16:59:29 -0500

    Added codewars tasks solutions

---
+++ js-tasks/js-tasks01.js
@@ -0,0 +1,29 @@
+// Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+
+
+// Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+const makeNegative = num => (num > 0) ? -num : num;
+
+
+// Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+const move = (position, roll) => roll * 2 + position;
+
+
+// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+const greet = (name, owner) => (name === owner) ? 'Hello boss' : 'Hello guest';
+
+
+// Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+const litres = time => Math.floor(time * 0.5);
+
+
+// Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+const lovefunc = (flower1, flower2) => (flower1 + flower2) % 2 === 1;
+
