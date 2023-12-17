025765cf8d7a63a535f33d6947544b70be4ef5b0
DarynaRiabova <125585715+DarynaRiabova@users.noreply.github.com>
2023-12-17 19:21:51 +0100

    Update js-tasks01.js

--- js-tasks/js-tasks01.js
+++ js-tasks/js-tasks01.js
@@ -2,6 +2,14 @@
 
 const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 
+//Daryna Riabova 
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  const sum = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return sum;
+}
+//or
+const goals = (a,b,c) => a + b + c;
+
 //Roman Slipak
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
@@ -15,6 +23,11 @@ function goals (laLiga, copaDelRey, championsLeague){
 // Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
 const makeNegative = num => (num > 0) ? -num : num;
+
+//Daryna Riabova 
+function makeNegative(num) {
+ return num <= 0 ? num : -num;
+}
 
 //Roman Slipak
 function makeNegative(num) {
@@ -32,6 +45,11 @@ function makeNegative(num) {
 
 const move = (position, roll) => roll * 2 + position;
 
+//Daryna Riabova 
+function move (position, roll) {
+ return position + roll * 2;
+}
+
 //Roman Slipak
 function move (position, roll) {
   return position + (roll * 2);
@@ -45,6 +63,11 @@ function move (position, roll) {
 // Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
 const greet = (name, owner) => (name === owner) ? 'Hello boss' : 'Hello guest';
+
+//Daryna Riabova 
+function greet (name, owner) {
+return name === owner ? 'Hello boss' : 	'Hello guest';
+}
 
 //Roman Slipak
 function greet (name, owner) {
@@ -63,6 +86,11 @@ function greet (name, owner) {
 
 const litres = time => Math.floor(time * 0.5);
 
+//Daryna Riabova 
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
 //Roman Slipak
 function litres(time) {
   return Math.floor(time * 0.5);
@@ -76,6 +104,11 @@ function litres(time) {
 // Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 
 const lovefunc = (flower1, flower2) => (flower1 + flower2) % 2 === 1;
+
+//Daryna Riabova
+function lovefunc(flower1, flower2){
+return (flower1 + flower2) % 2 === 1 ? true : false;
+}
 
 //Roman Slipak
 function lovefunc(flower1, flower2){
