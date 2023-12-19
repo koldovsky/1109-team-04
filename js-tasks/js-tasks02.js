c1f1ab606cf78f9b12cdd201c065af5501329b68
RomanSlipak <130825570+Brytalisk@users.noreply.github.com>
2023-12-19 13:27:39 +0300

    Update js-tasks02.js

--- js-tasks/js-tasks02.js
+++ js-tasks/js-tasks02.js
@@ -1,1 +1,40 @@
+// "Convert a string to an array" https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
+//Roman Slipak
+const stringToArray = (string) => string.split(" ");
+
+// "DNA to RNA Conversion" https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+//Roman Slipak
+function DNAtoRNA(dna) {
+  const structure = dna.split("");
+  for(let i = 0;i < structure.length;i++) {
+    if(structure[i] === "T") {
+      structure[i] = "U";
+    }
+  }
+  return structure.join("");
+}
+
+// "Find Maximum and Minimum" Values of a List https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+//Roman Slipak
+const min = function(list){
+    list.sort(compare);
+    return list[0];
+}
+
+const max = function(list){
+    list.sort(compare).reverse();
+    return list[0];
+}
+
+const compare = (a,b) => a - b;
+
+// "Smallest value of an array" https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+//Roman Slipak
+function min(arr, toReturn) {
+  const minNumber = Math.min(...arr);
+  return toReturn === "value" ? minNumber : arr.indexOf(minNumber);
+}
