8661bf5b69d6342b7b693b2caa7b68e4f9735770
Sviatoslav Nadorozhnyi <104847887+SvNadorozhnyi@users.noreply.github.com>
2023-12-22 10:48:54 -0500

    Update js-tasks03.js

--- js-tasks/js-tasks03.js
+++ js-tasks/js-tasks03.js
@@ -83,7 +83,7 @@ function buildFun(n) {
 //Sviatoslav Nadorozhnyi
 function buildFun(n){
   const res = []
-  for (let i = 0; i <= n; i++){
+  for (let i = 0; i < n; i++){
     res.push(function(){
       return i
     });
