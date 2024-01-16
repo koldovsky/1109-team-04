d9eec3dab4a6a5553d28d6124ba3cedc9bced072
toliaviking <151723174+toliaviking@users.noreply.github.com>
2024-01-16 12:40:23 +0000

    faq.js

--- js/faq.js
+++ js/faq.js
@@ -8,8 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
         const content = item.querySelector('.faq-accordion__content');
 
         header.addEventListener('click', () => {
-        
+  
             content.style.display = content.style.display === 'block' ? 'none' : 'block';
+
 
             const icon = header.querySelector('.faq-accordion__icon');
             icon.src = content.style.display === 'block' ? 'img/minus-icon.svg' : 'img/plus-icon.svg';
