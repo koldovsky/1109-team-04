b15a37366724ed26e9c19012a6ec6d3cace6031a
toliaviking <151723174+toliaviking@users.noreply.github.com>
2024-01-16 12:32:32 +0000

    faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,39 +1,18 @@
-const accordionItem = document.querySelectorAll(".faq-accordion__item");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const accordionItems = document.querySelectorAll('.faq-accordion__item');
 
 
+    accordionItems.forEach(item => {
+        const header = item.querySelector('.faq-accordion__header');
+        const content = item.querySelector('.faq-accordion__content');
 
+        header.addEventListener('click', () => {
+        
+            content.style.display = content.style.display === 'block' ? 'none' : 'block';
 
-accordionItem.forEach((item, index) => {
-    let accordionHeader = item.querySelector(".faq-accordion__header");
-    accordionHeader.addEventListener("click", () => {
-        item.classList.toggle("open");
-
-        let description = item.querySelector(".questions-text__block");
-        let icon = item.querySelector(".faq-accordion__icon");
-        if(item.classList.contains("open")) {
-            description.style.height = `${description.scrollHeight}px`
-            icon.src = "img/minus-icon.svg";
-            icon.alt = "dark-blue plus icon";
-        }else {
-            description.style.height = "0px";
-            icon.src = "img/plus-icon.svg";
-            icon.alt = "dark-blue plus icon";
-        }
-        console.log(description);
-        removeClassOpen(index);
-    })  
-})
-
-function removeClassOpen(index) {
-    accordionList.forEach((item2,index2) => {
-        if(index != index2) {
-            item2.classList.remove("open");
-            let description2 = item2.querySelector(".questions-text__block");
-            let icon2 = item2.querySelector(".faq-accordion__icon");
-            description2.style.height = "0px";
-            icon2.src = "img/plus-icon.svg";
-            icon2.alt = "dark-blue plus icon";
-        }
-    }) 
-   
-}
+            const icon = header.querySelector('.faq-accordion__icon');
+            icon.src = content.style.display === 'block' ? 'img/minus-icon.svg' : 'img/plus-icon.svg';
+        });
+    });
+});
