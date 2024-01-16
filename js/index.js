8aaec1e58eaf783d2f4c8101c01a749b94e8307b
toliaviking <151723174+toliaviking@users.noreply.github.com>
2024-01-16 11:25:04 +0000

    faq.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ document.addEventListener("partialsLoaded", () => {
     import("./pets-carousel.js");
     import("./services-carousel.js");
     import("./brands-carousel.js");
+    import("./faq.js");
     import("./contacts-accordion.js");
     import("./contacts-form-validations.js");
   });
